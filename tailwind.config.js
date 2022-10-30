609207e116b206a0ddd8ff6d0a8daf1174692a0c
suryapraca <82021996+suryapraca@users.noreply.github.com>
2022-10-30 18:49:22 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#827397',
+        secondary: '#E9D5CA',
+        dark: '#363062',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
